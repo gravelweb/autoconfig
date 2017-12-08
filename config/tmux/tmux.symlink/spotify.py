9813cef4475f444e00f39ac8e3ad3133761ab68e
Jonathan Gravel <gravelweb@gmail.com>
2017-12-08 10:47:47 -0500

    config: tmux
    Tmux config with vim bindings for navigation.

---
+++ config/tmux/tmux.symlink/spotify.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Query the spotify client for currently playing
+
+Help from http://stackoverflow.com/a/33923095/3213811
+
+TODO can this be event driven rather than polled?
+TODO volume control (C^upArrow, C^downArrow)
+
+<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+                      "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+<!-- GDBus 2.48.1 -->
+<node>
+  <interface name="org.freedesktop.DBus.Properties">
+    <method name="Get">
+      <arg type="s" name="interface_name" direction="in"/>
+      <arg type="s" name="property_name" direction="in"/>
+      <arg type="v" name="value" direction="out"/>
+    </method>
+    <method name="GetAll">
+      <arg type="s" name="interface_name" direction="in"/>
+      <arg type="a{sv}" name="properties" direction="out"/>
+    </method>
+    <method name="Set">
+      <arg type="s" name="interface_name" direction="in"/>
+      <arg type="s" name="property_name" direction="in"/>
+      <arg type="v" name="value" direction="in"/>
+    </method>
+    <signal name="PropertiesChanged">
+      <arg type="s" name="interface_name"/>
+      <arg type="a{sv}" name="changed_properties"/>
+      <arg type="as" name="invalidated_properties"/>
+    </signal>
+  </interface>
+  <interface name="org.freedesktop.DBus.Introspectable">
+    <method name="Introspect">
+      <arg type="s" name="xml_data" direction="out"/>
+    </method>
+  </interface>
+  <interface name="org.freedesktop.DBus.Peer">
+    <method name="Ping"/>
+    <method name="GetMachineId">
+      <arg type="s" name="machine_uuid" direction="out"/>
+    </method>
+  </interface>
+  <interface name="org.mpris.MediaPlayer2">
+    <method name="Raise"/>
+    <method name="Quit"/>
+    <property type="b" name="CanQuit" access="read"/>
+    <property type="b" name="CanRaise" access="read"/>
+    <property type="b" name="HasTrackList" access="read"/>
+    <property type="s" name="Identity" access="read"/>
+    <property type="s" name="DesktopEntry" access="read"/>
+    <property type="as" name="SupportedUriSchemes" access="read"/>
+    <property type="as" name="SupportedMimeTypes" access="read"/>
+  </interface>
+  <interface name="org.mpris.MediaPlayer2.Player">
+    <method name="Next"/>
+    <method name="Previous"/>
+    <method name="Pause"/>
+    <method name="PlayPause"/>
+    <method name="Stop"/>
+    <method name="Play"/>
+    <method name="Seek">
+      <arg type="x" name="Offset" direction="in"/>
+    </method>
+    <method name="SetPosition">
+      <arg type="o" name="TrackId" direction="in"/>
+      <arg type="x" name="Position" direction="in"/>
+    </method>
+    <method name="OpenUri">
+      <arg type="s" name="Uri" direction="in"/>
+    </method>
+    <signal name="Seeked">
+      <arg type="x" name="Position"/>
+    </signal>
+    <property type="s" name="PlaybackStatus" access="read"/>
+    <property type="s" name="LoopStatus" access="readwrite"/>
+    <property type="d" name="Rate" access="readwrite"/>
+    <property type="b" name="Shuffle" access="readwrite"/>
+    <property type="a{sv}" name="Metadata" access="read"/>
+    <property type="d" name="Volume" access="readwrite"/>
+    <property type="x" name="Position" access="read"/>
+    <property type="d" name="MinimumRate" access="read"/>
+    <property type="d" name="MaximumRate" access="read"/>
+    <property type="b" name="CanGoNext" access="read"/>
+    <property type="b" name="CanGoPrevious" access="read"/>
+    <property type="b" name="CanPlay" access="read"/>
+    <property type="b" name="CanPause" access="read"/>
+    <property type="b" name="CanSeek" access="read"/>
+    <property type="b" name="CanControl" access="read"/>
+  </interface>
+</node>
+
+"""
+
+import dbus
+import sys
+
+def get_spotify_bus():
+    session_bus = dbus.SessionBus()
+    return session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                  "/org/mpris/MediaPlayer2")
+
+def display():
+    bus = get_spotify_bus()
+
+    # Update properties
+    properties = get_spotify_properties(bus)
+    player_properties = properties.GetAll("org.mpris.MediaPlayer2.Player")
+    metadata = player_properties['Metadata']
+    playback_status = player_properties['PlaybackStatus']
+
+    title = metadata['xesam:title'].encode('utf-8')
+    artists = ', '.join(metadata['xesam:artist']).encode('utf-8')
+    status = '♪' if playback_status == 'Playing' else ' '
+    print("{status} {title} - {artists}".format(**locals()))
+
+def get_spotify_properties(bus):
+    return dbus.Interface(bus, "org.freedesktop.DBus.Properties")
+
+def control_player(action):
+    bus = get_spotify_bus()
+
+    player = get_player_proxy(bus)
+
+    if action == 'ToggleStatus':
+        properties = get_spotify_properties(bus)
+        player_properties = properties.GetAll("org.mpris.MediaPlayer2.Player")
+        playback_status = player_properties['PlaybackStatus']
+        action = 'Play' if playback_status == 'Paused' else 'Pause'
+
+    try:
+        getattr(player, action)()
+    except dbus.exceptions.DBusException:
+        print("Unsupported action {}".format(action))
+
+def get_player_proxy(bus):
+    return dbus.Interface(bus, "org.mpris.MediaPlayer2.Player")
+
+def main():
+    if len(sys.argv) == 1:
+        display()
+    else:
+        control_player(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
---
+++ config/tmux/tmux.symlink/theme.sh
@@ -0,0 +1,44 @@
+# colours
+tm_colour_active=colour120
+tm_colour_inactive=colour241
+tm_colour_feature=colour13
+tm_colour_music=colour147
+tm_colour_active_border=colour198
+
+# window title colours
+set-window-option -g window-status-current-fg $tm_colour_active
+set-window-option -g window-status-current-bg default
+set-window-option -g window-status-current-format "#[bold]#I #W"
+
+# pane border
+set-option -g pane-border-fg $tm_colour_inactive
+set-option -g pane-active-border-fg $tm_colour_active_border
+
+# message text
+set-option -g message-bg default
+set-option -g message-fg $tm_colour_active
+
+# pane number display
+set-option -g display-panes-active-colour $tm_colour_active
+set-option -g display-panes-colour $tm_colour_inactive
+
+# clock
+set-window-option -g clock-mode-colour $tm_colour_active
+
+# status bar
+set -g status-left-length 32
+set -g status-right-length 150
+set -g status-interval 5
+
+set-option -g status-fg $tm_colour_active
+set-option -g status-bg default
+set-option -g status-attr default
+
+tm_spotify="#[fg=$tm_colour_music]#(~/.tmux/spotify.py)"
+
+tm_data="#[fg=$tm_colour_inactive] %R %d %b"
+tm_host="#[fg=$tm_colour_feature,bold]#h"
+tm_session_name="#[fg=$tm_colour_feature,bold]$tm_icon #S"
+
+set -g status-left $tm_session_name' '
+set -g status-right $tm_spotify' '$tm_date' '$tm_host
