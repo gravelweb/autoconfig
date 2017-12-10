a20115a1bde75de65c65ee5a38e60131d3506648
Jonathan Gravel <gravelweb@gmail.com>
2017-12-09 22:46:35 -0800

    Prompt when symlinks need to be installed

--- install-scripts/symlinks.sh
+++ install-scripts/symlinks.sh
@@ -5,8 +5,41 @@ for symlink in "${symlinkfiles[@]}"; do
     basename=${symlink##*/}
     dest=~/.${basename%.*}
     if [[ -e "${dest}" ]]; then
-        echo "Skipping override of ${dest}..."
-        continue
+        if [[ "$(readlink -- "${dest}")" == "${symlink}" ]]; then
+            echo "Symlink already set: ${dest}"
+            continue
+        fi
+        echo "Symlink ${dest} exists: "
+        echo "options:"
+        echo "  * [o]verride"
+        echo "  * [b]ackup and override"
+        echo "  * [s]kip"
+        echo -n "Choose an option: "
+        done="false"
+        skip="false"
+        while [[ "${done}" == "false" ]]; do
+            read symlink__option
+            done="true"
+            case "${symlink__option}" in
+                o|override)
+                    rm -rf "${dest}"
+                    ;;
+                b|backup)
+                    mv "${dest}" "${dest}.bkp"
+                    ;;
+                s|skip)
+                    skip="true"
+                    ;;
+                *)
+                    echo "Unknown option: ${symlink__option}"
+                    done="false"
+                    ;;
+            esac
+        done
+        if [[ "${skip}" == "true" ]]; then
+            echo "Skipped ${dest}"
+            continue
+        fi
     fi
     ln -vs ${symlink} ${dest}
 done
