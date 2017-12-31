45782bcea32e474cd49fa2431cc039a8120c275b
Jonathan Gravel <gravelweb@gmail.com>
2017-12-31 15:48:39 -0800

    Add support for snap.classic prereqs

--- install-scripts/prereqs.sh
+++ install-scripts/prereqs.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-suffix=
-installer=
+function install_prereqs {
+    local suffix=$1
+    local installer=($2)
+
+    local prereqfiles=($(find "$SOURCE" "${findargs[@]}" -name prereqs.${suffix} -print))
+    local prereqs=()
+    local prereq=
+    for prereq in "${prereqfiles[@]}"; do
+        prereqs+=($(grep -v '^[\s]*#' ${prereq}))
+    done
+
+    if [[ ${#prereqs[@]} == 0 ]]; then
+        echo "No ${suffix} prereqs found"
+        return 1
+    fi
+
+    echo "Installing ${suffix} prereqs"
+    sudo "${installer[@]}" "${prereqs[@]}"
+}
+
+prereqs_installed=
+
 if which apt >/dev/null 2>&1; then
-    # debian
-    suffix=apt
-    installer=(apt install --yes)
+    if install_prereqs apt "apt install --yes"; then
+        prereqs_installed=true
+    fi
 fi
 
-if [[ -z "{suffix}" ]]; then
-    echo "Error: Unknown distribution, can't install prerequisites." >&2
+if which snap >/dev/null 2>&1; then
+    if install_prereqs snap.classic "snap install --classic"; then
+        prereqs_installed=true
+    fi
+fi
+
+if [[ -z "${prereqs_installed}" ]]; then
+    echo "Error: Couldn't find any prerequisites, likely cause is unknown distribution." >&2
     exit 1
 fi
-
-prereqfiles=($(find "$SOURCE" "${findargs[@]}" -name prereqs.${suffix} -print))
-prereqs=()
-for prereq in "${prereqfiles[@]}"; do
-    prereqs+=($(grep -v '^[\s]*#' ${prereq}))
-done
-
-sudo "${installer[@]}" "${prereqs[@]}"
