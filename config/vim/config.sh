715a46dd8e1139c8057f5bb9f55d29c3681fb40b
Jonathan Gravel <gravelweb@gmail.com>
2018-10-21 10:15:54 -0400

    Create vimpy3 script
    Syntastic looks for 'python' executable, which is currently reserved for
    python2.7. The vimpy3 executabl will use the python3 virtualenv, which
    has 'python3' aliased to 'python'.

--- config/vim/config.sh
+++ config/vim/config.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+set -e
+
 # directory for vim swap files
 mkdir -p ~/tmp
+
+# Create python3 environment for syntastic. Hopefully python3 becomes the
+# default soon enough, at which point this extra config can be dropped.
+venvdir=~/.python3venv
+if [[ ! -d "${venvdir}" ]]; then
+    virtualenv -p python3 "${venvdir}"
+fi
+pushd "${venvdir}"
+source bin/activate
+pip install pylint pytest
+deactivate
+popd
--- config/vim/vim.symlink/ftplugin/python.vim
+++ config/vim/vim.symlink/ftplugin/python.vim
@@ -2,3 +2,6 @@ setlocal expandtab
 setlocal tabstop=4
 setlocal softtabstop=4
 setlocal shiftwidth=4
+
+" pylint disable
+let g:syntastic_python_pylint_quiet_messages = { "regex": "missing-docstring" }
