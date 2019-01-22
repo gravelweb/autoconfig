ee5d0d327a29b76406ddf3bcc89b72047383f42c
Jonathan Gravel <gravelweb@gmail.com>
2019-01-21 20:19:58 -0500

    vim: Install ncm2 python plugin

--- config/vim/config.sh
+++ config/vim/config.sh
@@ -24,5 +24,6 @@ pushd "${SCRIPT_DIR}/vim.symlink/pack/bundle/start"
 ./fzf/install --key-bindings --completion --update-rc
 popd
 
-# Install neovim python client
+# Install ncm2 dependencies
 pip3 install neovim
+pip3 install jedi        # ncm2-jedi dependency
